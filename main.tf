5a383cdc276e5858b49bd246844ce5babc20ebd1
Kaliyappan1 <yappank17@gmail.com>
2025-07-18 21:06:34 +0530

    updated code

--- main.tf
+++ main.tf
@@ -104,6 +104,7 @@ resource "aws_instance" "splunk_cluster" {
     RunQuotaHours = var.quotahours
     Category      = var.category
     PlanStartDate = var.planstartdate
+    hoursperday   = var.hoursperday
   }
 }
 
